66afda84e58979298813c935f5b88a2d89475b2b
Fredrik Larsson <fl@larssonconsulting.net>
2023-10-06 15:39:54 +0200

    init

---
+++ src/WorkManSoft.SaaS.Core/WorkManSoft.SaaS.Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkManSoft.SaaS.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
