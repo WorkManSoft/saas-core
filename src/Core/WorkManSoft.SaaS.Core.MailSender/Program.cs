895851e8557d7c53f1eca999bfb5f9e56428d82e
Fredrik Larsson <fl@larssonconsulting.net>
2024-06-25 17:40:17 +0200

    x

---
+++ src/Core/WorkManSoft.SaaS.Core.MailSender/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkManSoft.SaaS.Core.MailSender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
